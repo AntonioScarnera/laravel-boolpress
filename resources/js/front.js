2c40b2f91a1d344df6fba92656c00d5d5128ad55
AntonioScarnera <scarnerantonio07@gmail.com>
2022-06-27 15:05:55 +0200

    boolpress

---
+++ resources/js/front.js
@@ -0,0 +1,13 @@
+require('./bootstrap');
+
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.Vue = require('vue');
+
+import App from './views/App';
+
+ const app = new Vue({
+	el: '#root',
+	render: h => h(App), // renderizziamo App all'avvio di Vue
+});
---
+++ resources/js/views/App.vue
@@ -0,0 +1,16 @@
+<template>
+   <main class="container mt-3">
+    <app-main />
+  </main>
+</template>
+
+<script>
+import AppMain from '../components/AppMain.vue';
+export default {
+    name: "App",
+    components: {
+        AppMain
+    }
+
+}
+</script>
